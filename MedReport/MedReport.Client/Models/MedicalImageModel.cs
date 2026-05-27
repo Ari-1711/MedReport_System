b305bc401af6ee1e7658ab21cbe9760bea41769a
Ari-1711 <arihermawan006@gmail.com>
2026-05-27 16:03:44 +0700

    feat: inisialisasi MedicalImageModel

---
+++ MedReport/MedReport.Client/Models/MedicalImageModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MedReport.Client.Models
+{
+    internal class MedicalImageModel
+    {
+        // Menyimpan lokasi gambar asli (High Res) untuk Report PDF
+        public string OriginalPath { get; set; }
+        // Menyimpan gambar kecil (Low Res) untuk UI
+        public System.Windows.Media.Imaging.BitmapImage Thumbnail { get; set; }
+    }
+}
