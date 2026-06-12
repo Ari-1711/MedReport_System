ab59a867571e510d0ef4fac93df508d0b93e9297
Ari-1711 <arihermawan006@gmail.com>
2026-06-12 10:59:48 +0700

    feat: add ImageHelper utility for non-locking bitmap loading

---
+++ MedReport/MedReport.Client/Utilities/ImageHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace MedReport.Client.Utilities
+{
+    public static class ImageHelper
+    {
+        public static BitmapImage LoadThumbnailWithoutLocking(string filePath)
+        {
+            if (string.IsNullOrWhiteSpace(filePath) || !File.Exists(filePath))
+                return null;
+
+            var bitmap = new BitmapImage();
+
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri(filePath);
+            bitmap.DecodePixelWidth = 150;
+            bitmap.CacheOption = BitmapCacheOption.OnLoad;
+            bitmap.EndInit();
+
+            if (bitmap.CanFreeze) bitmap.Freeze();
+
+            return bitmap;
+        }
+    }
+}
